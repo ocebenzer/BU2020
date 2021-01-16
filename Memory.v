a39c747f5c7577e277ea69a171e5d93a2c3208ee
ocebenzer <53233055+ocebenzer@users.noreply.github.com>
2021-01-16 16:43:25 +0300

    memory done

--- Memory.v
+++ Memory.v
@@ -1,23 +1,39 @@
-module Memory(input[11:0] address_bus, inout[15:0] databus, input wire clk, write_mode);
+module Memory(input[11:0] address_bus, inout[15:0] data_bus, input clk, write_mode);
 
 	// 4KB Memory, 4x1KB modules, first module is instruction module, rest 3 are data modules
-	reg[2047:0][15:0] data = 0;
-	wire module_no = address_bus[11:10];
-	wire relative_address_low = address_bus[9:0];
-	wire relative_address_high = address_bus[9:0] + 1;
+	reg[511:0][15:0] data_instruction = 0;
+	reg[511:0][15:0] data1 = 0;
+	reg[511:0][15:0] data2 = 0;
+	reg[511:0][15:0] data3 = 0;
+	
+	initial begin
+		data3[510] <= 16'habcd;
+	end
 
-	//data[[module_no][relative_address+1]:[module_no][relative_address]] gives us the 16 bit we address
+	reg[15:0] data_bus_read;
+
+	assign data_bus = write_mode ? 'bz : data_bus_read;
 
 	always@(posedge clk) begin
 		case (write_mode)
 			1'b0: begin
 				//read mode
-				//databus <= data[[address_bus[11:10]][address_bus[9:0]+1]:[address_bus[11:10]][address_bus[9:0]]];
-				end
+				case(address_bus[11:10])
+					2'b00: data_bus_read <= data_instruction[address_bus[9:1]];
+					2'b01: data_bus_read <= data1[address_bus[9:1]];
+					2'b10: data_bus_read <= data2[address_bus[9:1]];
+					2'b11: data_bus_read <= data3[address_bus[9:1]];
+				endcase
+			end
 			1'b1: begin
 				//write mode
-				data[address_bus[11:1]] <= databus;
-				end
+				case(address_bus[11:10])
+					2'b00: data_instruction[address_bus[9:1]] <= data_bus;
+					2'b01: data1[address_bus[9:1]] <= data_bus;
+					2'b10: data2[address_bus[9:1]] <= data_bus;
+					2'b11: data3[address_bus[9:1]] <= data_bus;
+				endcase
+			end
 		endcase
 	end
 endmodule
--- main.v
+++ main.v
@@ -6,29 +6,59 @@ reg clk;
 wire[15:0] instruction_bus;
 assign instruction_bus = 16'b1100001000110010; // Move Immediate, D0 50(decimal)
 
-wire[15:0] data_bus;
+reg[15:0] data_bus_reg;
+reg[11:0] address_bus_reg;
+reg write_mode_reg;
+
+wire[15:0]data_bus;
 wire[11:0] address_bus;
+wire write_mode;
 
-assign data_bus = 16'habcd;
-assign address_bus = 12'hffe;
+always @(posedge clk) begin
+	data_bus_reg <= data_bus;
+	address_bus_reg <= address_bus;
+end
 
-Memory memory(address_bus, data_bus, clk, 1'b1);
+assign data_bus = write_mode ? data_bus_reg : 'bz;
+assign address_bus = address_bus_reg;
+assign write_mode = write_mode_reg;
+
+
+Memory memory(address_bus, data_bus, clk, write_mode);
 BU2020 cpu(clk, instruction_bus);
 
 initial begin
 	$dumpfile("TimingDiagram.vcd");
-	$dumpvars(0, memory, clk);
+	$dumpvars(0, memory, data_bus, address_bus, data_bus_reg, address_bus_reg, clk);
 
-	#400
+ 	write_mode_reg <= 1'b1;
+
+	data_bus_reg <= 16'haaaa;
+	address_bus_reg <= 12'hffe;
+	#4
+	data_bus_reg <= 16'hbbbb;
+	address_bus_reg <= 12'hffc;
+	#4
+	data_bus_reg <= 16'hcccc;
+	address_bus_reg <= 12'hffa;
+	#4
+	data_bus_reg <= 16'hdddd;
+	address_bus_reg <= 12'hff8;
+	#8
+	
+ 	write_mode_reg <= 1'b0;
+	address_bus_reg <= 12'hffc;
+
+	#20
 
 	$finish;
 end
 
 always begin	
 	clk = 0;
-	#100;
+	#1;
     clk = 1;
-    #100;
+    #1;
 end
 
 endmodule
